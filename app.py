545fbfa75a8bf8f9080b9efe7d4dd685e91845f1
jonathanha1e <jonathan.hale@rocketmail.com>
2025-07-08 17:11:51 -0700

    fixed flask host and port

--- app.py
+++ app.py
@@ -17,5 +17,8 @@ def flow():
         data = json.load(f)
     return jsonify(data)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
